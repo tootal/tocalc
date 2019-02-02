47034cdf2bb0a1e6f02ffcb4d6da972fa09ae312
tootal <huang_zhi_quan@163.com>
2019-02-02 15:33:41 +0800

    create tomath.cpp tomath.h main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+#include "tomath.h"
+
+using namespace std;
+
+int main(){
+	// cout<<"hello world!\n";
+	
+
+	return 0;
+}
---
+++ tomath.h
@@ -0,0 +1,15 @@
+/*
+Create by tootal at 2019.2.2
+Compiler version: MinGW-W64 gcc 8.1.0
+Compile and run command: 
+
+g++ -o main main.cpp tomath.cpp & main
+
+*/
+#ifndef _TOMATH_H
+#define _TOMATH_H
+
+
+
+
+#endif
